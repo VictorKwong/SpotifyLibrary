f5d8b893c36a1af2589d34fe3b4e7df90473f18b
Victor Wong <victorkaihong@gmail.com>
2020-08-27 20:02:59 -0400

    adding features

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 const musicStage = {};
 
 musicStage.userChoice = function (){
-    $('.songCircle').on('click', function(){
 
+    console.log('hello');
+    let audioElem = document.getElementById("1");
+    $(window).keypress(function(e) {
+        //if user press spacebar
+        if(this.paused && e.keyCode == 32){
+            this.play();
+        }else if(this.play && e.keyCode == 32){
+            this.pause();
+        }
     })
 }
-
-
 musicStage.init = () => {
-
+    musicStage.userChoice();
 }
 
 $(document).ready(musicStage.init());
